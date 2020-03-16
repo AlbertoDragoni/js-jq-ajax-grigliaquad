547e14cf97bb67e32cda2c5b95b053e2f3e6d258
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-16 16:35:17 +0100

    Esercizio ok senza hb

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(document).ready(function(){
 
+    $('.square').click(function(){
+        var questa = $(this);
+        $.ajax({
+                url: 'https://flynn.boolean.careers/exercises/api/random/int',
+                method: 'GET',
+                success: function (data) {
+                    var numeroPC = data.response;
+                    if (numeroPC > 5) {
+                        questa.css('background-color', 'yellow');
+                    } else if (numeroPC <= 5){
+                        questa.css('background-color', 'green');
+                    }
+                    questa.children('p').text(numeroPC);
+                },
+                error: function () {
+
+                }
+            });
+    });
+
+
+
+
+});
